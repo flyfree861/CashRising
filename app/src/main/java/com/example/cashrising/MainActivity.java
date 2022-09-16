1e271d8af170f0d6f428e48b69debf64abc6db4e
flyfree861 <mazzuoccolo.fabio@gmail.com>
2022-09-16 23:31:08 +0200

    Prima versione creato item e bottom navigation menu con fragment per ogni item

---
+++ app/src/main/java/com/example/cashrising/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.cashrising;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity
+{
+    BottomNavigationView bottomNavigationMenuView;
+    HomeFragment homeFragment = new HomeFragment();
+    MoneyFragment moneyFragment = new MoneyFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigationMenuView = findViewById( R.id.bottom_navigation_menu);
+        getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame, homeFragment).commit();
+
+        bottomNavigationMenuView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId())
+            {
+                case R.id.item_home:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame, homeFragment).commit();
+                    return true;
+
+                case R.id.item_money:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame, moneyFragment).commit();
+                    return true;
+
+                case R.id.item_setting:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame, settingsFragment).commit();
+                    return true;
+            }
+            return false;
+        });
+    }
+
+
+}
